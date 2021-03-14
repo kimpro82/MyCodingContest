4198fcb797ee178091631bf5b95e42a2688f8bbe
Kangrok Kim <kimpro1010@gmail.com>
2021-03-14 18:39:59 +0900

    Create B.cs

---
+++ AtCoder/Contest/Beginner0195/B.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    int A = input[0];
+    int B = input[1];
+    int W = input[2] * 1000;
+
+    int min = W / B;
+    int max = W / A;
+
+    int min2;
+    if (W % B == 0) {
+      min2 = min;    
+    } else {
+      min2 = min + 1;
+    }
+
+    if (W - A * min2 > B) {
+      Console.WriteLine(min2 + " " + max);
+    }
+    else if (W - B * max < 0 ) {
+      Console.WriteLine(min2 + " " + max);
+    }
+    else {
+      Console.WriteLine("UNSATISFIABLE");
+    }
+
+  }
+}

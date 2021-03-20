81b47aac05da5cce860ec7ff4a3572cab15d2950
Kangrok Kim <kimpro@uebermensch.tech>
2021-03-21 05:07:13 +0900

    Update : AtCoder Beginner Contest 195 / B, C

--- AtCoder/Contest/Beginner0195/B/B2/B2.cs
+++ AtCoder/Contest/Beginner0195/B/B2/B2.cs
@@ -2,10 +2,12 @@ using System;
 using System.Linq;
 
 
-namespace ABC0195B
+namespace ABC0195B2
 {
-  class MainClass {
-    public static void Main (string[] args) {
+  class MainClass
+  {
+    public static void Main (string[] args)
+    {
 
       var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
@@ -16,20 +18,20 @@ namespace ABC0195B
       int min = W / B;
       int max = W / A;
 
-      int min2;
+      int min2;                                  // min2 is not yet guaranteed if valid
       if (W % B == 0) {
         min2 = min;    
       } else {
         min2 = min + 1;
       }
 
-      if (W - A * min2 > B) {
+      // if (W - A * min2 > B || W - B * max < 0)   // || : or, && : and
+      if (W - A * min2 >= A || W - B * max <= 0)
+      {
         Console.WriteLine(min2 + " " + max);
       }
-      else if (W - B * max < 0 ) {
-        Console.WriteLine(min2 + " " + max);
-      }
-      else {
+      else
+      {
         Console.WriteLine("UNSATISFIABLE");
       }
 

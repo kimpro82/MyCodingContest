26549068c758933ca31e383fdf05fbad27568e30
Kangrok Kim <kimpro@uebermensch.tech>
2021-03-30 03:02:33 +0900

    Creat A.cs

--- CodeForces/Contest/Round0711/A/A.cs
+++ CodeForces/Contest/Round0711/A/A.cs
@@ -14,26 +14,50 @@ namespace A
                 75
             */
 
-            int T = int.Parse(Console.ReadLine());              // 3
+            int T = int.Parse(Console.ReadLine());                          // 3
 
             // read each case and print its output
             for (int t = 1; t <= T; t++)
             {
-                
-                // 1. read n
-                string[] input = Console.ReadLine();            // 1 <= n <= 18
-                int len = input.Length;
 
+                Int64 n = Int64.Parse(Console.ReadLine());                  // 1 <= n <= 10^18
+                Int64 min = 0;                                              // for calculating gcd
 
-                // 2. gcd operation
+                // loop for finding next n (n++) until gcdsum > 1
+                while (min < 2)
+                {
+                    // 1. read n
+                    int len = (char)(n.ToString()).Length;
 
+                    // 2. gcdSum operation
+                    int sum = 0;
+                    for (int i = 0; i < len; i++)
+                    {
+                        sum += int.Parse(n.ToString()[i].ToString());       // Parse() only recieves string
+                        Console.WriteLine("{0} {1} {2} ", n, i, sum);       // test
+                    }
 
-                // test
-                Console.WriteLine("case {0} : {1}", t, len);
+                    // 3. gcd operation
+                    Int64 max = Math.Max(n, sum);
+                    min = Math.Min(n, sum);
+                    Int64 mod = max;
+                    while(mod != 0)
+                    {
+                        mod = max % min;
+                        max = min;
+                        min = mod;
+                        Console.WriteLine("{0} {1} {2} ", max, min, mod);   // test
+                    }
 
-                // 3. output
+                    n += 1;
 
-            }
+                } // the end of while loop
+
+                // 4. output
+                Console.WriteLine("case {0} : {1} {2} ", t, n - 1, min);   // test
+
+            } // the end of t loop
+
         }
     }
 }

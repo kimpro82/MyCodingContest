e57ec9aa1a0b00ad1ce602aae408027ea734e13d
Kangrok Kim <kimpro@uebermensch.tech>
2021-03-29 23:59:21 +0900

    CodeCraft-21 and Codeforces Round #711 (Div. 2)

---
+++ CodeForces/Contest/Round0711/A/A.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Sample Input
+                3
+                11
+                31
+                75
+            */
+
+            int T = int.Parse(Console.ReadLine());              // 3
+
+            // read each case and print its output
+            for (int t = 1; t <= T; t++)
+            {
+                
+                // 1. read n
+                string[] input = Console.ReadLine();            // 1 <= n <= 18
+                int len = input.Length;
+
+
+                // 2. gcd operation
+
+
+                // test
+                Console.WriteLine("case {0} : {1}", t, len);
+
+                // 3. output
+
+            }
+        }
+    }
+}

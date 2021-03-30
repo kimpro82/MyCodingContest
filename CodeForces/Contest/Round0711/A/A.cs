d0c9dc38d9371ae7388da93f79297c9009ca67fb
Kangrok Kim <kimpro@uebermensch.tech>
2021-03-31 06:20:26 +0900

    Update A.cs and README.md

--- CodeForces/Contest/Round0711/A/A.cs
+++ CodeForces/Contest/Round0711/A/A.cs
@@ -21,10 +21,10 @@ namespace A
             {
 
                 Int64 n = Int64.Parse(Console.ReadLine());                  // 1 <= n <= 10^18
-                Int64 min = 0;                                              // for calculating gcd
+                Int64 max = 0;                                              // for getting gcd from while loop
 
                 // loop for finding next n (n++) until gcdsum > 1
-                while (min < 2)
+                while (max < 2)
                 {
                     // 1. read n
                     int len = (char)(n.ToString()).Length;
@@ -34,27 +34,29 @@ namespace A
                     for (int i = 0; i < len; i++)
                     {
                         sum += int.Parse(n.ToString()[i].ToString());       // Parse() only recieves string
-                        Console.WriteLine("{0} {1} {2} ", n, i, sum);       // test
+                        // Console.WriteLine("gcdSum op. : {0} {1} {2} ", n, i, sum);       // test
                     }
 
                     // 3. gcd operation (Euclidean algorithm)
-                    Int64 max = Math.Max(n, sum);
-                    min = Math.Min(n, sum);
+                    max = Math.Max(n, sum);
+                    Int64 min = Math.Min(n, sum);
                     Int64 mod = max;
                     while(mod != 0)
                     {
                         mod = max % min;
+                        // Console.WriteLine("gcd op. : {0} {1} {2} ", max, min, mod);   // test
                         max = min;
                         min = mod;
-                        Console.WriteLine("{0} {1} {2} ", max, min, mod);   // test
                     } // gcd = min when escape while loop
 
-                    n += 1;
+                    n += 1; // should be subtracted 1 when escape while loop
 
                 } // the end of while loop
 
                 // 4. output
-                Console.WriteLine("case {0} : {1} {2} ", t, n - 1, min);   // test
+                // Console.WriteLine("case {0} : {1} {2} ", t, max, n - 1);   // test
+                Console.WriteLine(n - 1);
+
 
             } // the end of t loop
 
--- README.md
+++ README.md
@@ -7,10 +7,9 @@ https://codingcompetitions.withgoogle.com/codejam
 - Qualification Round 2021 (2021.03.26 22:00 ~ 03.28 04:00, 30 hrs)
 - Qualification Round 2020 (2020.04.04 08:00 ~ 04.05 11:00, 27 hrs)
 
-
 ### CodeForces
 https://codeforces.com/contests
-- Codeforces Round #708 (Div. 2) (2021.03.22 23:35, 2 hrs) (Comming)
+- CodeCraft-21 and Codeforces Round #711 (Div. 2) (2021.03.29 23:35, 2 hrs)
 - Codeforces Round #638 (Div. 2) (2020.05.01 23:35, 2.5 hrs)
 - Codeforces Round #637 (Div. 2) (2020.04.23 23:35, 2.5 hrs)
 - Codeforces Round #635 (Div. 2) (2020.04.15 23:35, 2.5 hrs)
@@ -19,10 +18,8 @@ https://codeforces.com/contests
 - April Fools Day Contest 2020 (2020.04.01 23:35, 2 hrs)
 - Codeforces Round #628 (Div. 2) (2020.03.14 23:35, 2 hrs)
 
-
 ### AtCoder
 https://atcoder.jp/
-- AtCoder Beginner Contest 197 (2021.03.27 21:00, 100 minutes) (Comming)
 - AtCoder Beginner Contest 196 (2021.03.20 21:00, 100 minutes)
 - AtCoder Beginner Contest 195 (2021.03.13 21:00, 100 minutes)
 - AtCoder Regular Contest 107 (2020.10.31 21:00, 100 minutes)
@@ -30,7 +27,6 @@ https://atcoder.jp/
 - AtCoder Beginner Contest 163 (2020.04.19 21:00, 100 minutes)
 - AtCoder Beginner Contest 162 (2020.04.12 21:00, 100 minutes)
 - AtCoder Beginner Contest 161 (2020.04.04 21:00, 100 minutes)
-
 
 ### Oncoder Official Coding Test
 https://www.oncoder.com/

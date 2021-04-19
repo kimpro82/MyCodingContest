8f83d9f8dd2de353225b03799fdd7a7c320ce316
Kangrok Kim <kimpro@uebermensch.tech>
2021-04-19 23:16:58 +0900

    Update A.cs

---
+++ CodeForces/Contest/Round0716/A/A.cs
@@ -0,0 +1,55 @@
+﻿// Codeforces Round #716 (Div. 2)
+// 2021.04.19(Mon) 22:35, 2.25 hrs
+
+// A. Perfectly Imperfect Array
+
+using System;
+using System.Linq;
+
+namespace CFR716Div2A
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+            /* Sample Input & Output
+                2
+                3
+                1 5 4
+                2
+                100 10000
+
+                YES
+                NO
+            */
+
+            int T = int.Parse(Console.ReadLine());                                  // 2
+
+            // Read each case and print its output
+            for (int t = 1; t <= T; t++)
+            {
+                
+                int n = int.Parse(Console.ReadLine());                              // 3
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray(); // 1 5 4
+
+                // Calculate product and its sqrt
+                int product = 1;
+                for (int i = 0; i < n; i++)
+                {
+                    product *= input[i];
+                }
+                float sqrt = (float) Math.Sqrt(product);
+
+                // Output
+                if (sqrt != Math.Floor(sqrt))
+                {
+                    Console.WriteLine("YES");
+                } else {
+                    Console.WriteLine("NO");
+                }
+
+            } // The end of t loop
+        }
+    }
+}
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ https://codingcompetitions.withgoogle.com/codejam
 https://codeforces.com/contests
 - Codeforces Round #718 (Div. 2) (2021.04.23 23:35, 2.25 hrs) (Comming)
 - Codeforces Round #717 (Div. 2) (2021.04.21 22:35, 2 hrs) (Comming)
-- Codeforces Round #716 (Div. 2) (2021.04.19 22:35, 2.25 hrs) (Comming)
+- Codeforces Round #716 (Div. 2) (2021.04.19 22:35, 2.25 hrs)
 - Codeforces Round #715 (Div. 2) (2021.04.16 23:35, 2.25 hrs)
 - CodeCraft-21 and Codeforces Round #711 (Div. 2) (2021.03.29 23:35, 2 hrs)
 - Codeforces Round #710 (Div. 3) (2021.03.25 23:35, 2 hrs)

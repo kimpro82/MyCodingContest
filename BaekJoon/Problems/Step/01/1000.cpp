75491996cbd13c5e958e2da618aecbb296e2c6d4
Kangrok Kim <kimpro@uebermensch.tech>
2021-05-13 23:12:17 +0900

    Create 1000.cpp and 1000.py

---
+++ BaekJoon/Problems/Step/01/1000.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>    // for .getline()
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}

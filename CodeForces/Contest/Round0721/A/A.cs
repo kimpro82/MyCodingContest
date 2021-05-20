da40c188da35a3a890fc1dd0204776ba387affcc
Kangrok Kim <kimpro@uebermensch.tech>
2021-05-20 23:38:09 +0900

    Create A.cs

---
+++ CodeForces/Contest/Round0721/A/A.cs
@@ -0,0 +1,29 @@
+﻿// Codeforces Round #721 (Div. 2)
+// 2021.05.20(Thu) 23:35, 2.00 hrs
+
+// A. And Then There Were K
+
+using System;
+
+namespace CFR721A
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+            /* Sample Input & Output
+                3
+                2
+                5
+                17
+
+                1
+                3
+                15
+            */
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

2d6d3799a8a46b3b4a9b6ad47dd0c6a10d57d01c
Kangrok Kim <kimpro1010@gmail.com>
2021-05-24 02:48:02 +0900

    AtCoder Beginner Contest 202 (#81)
    * Create A.cs, B.cs and C.cs
    * Update README.md

---
+++ AtCoder/Contest/Beginner0202/B/B.cs
@@ -0,0 +1,48 @@
+﻿// AtCoder Beginner Contest 202
+// 2021-05-22(Sat) 21:00 - 22:40, 100 minutes
+ 
+// B - 180°
+ 
+/* Sample Input & Output
+0601889
+6881090
+ 
+86910
+01698
+ 
+01010
+01010
+*/
+ 
+using System;
+using System.Text;  // for using StringBuilder;
+ 
+namespace ABC0202B
+{
+    class MainClass
+    {
+        public static void Main (string[] args) 
+        {
+            // Read data
+            StringBuilder s = new StringBuilder(Console.ReadLine());    // 0601889
+            int n = s.Length;                                           // 7
+ 
+            // Rotate S 180 degrees
+            StringBuilder sReverse = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                if (s[n-i-1] == '6')
+                {
+                    s[n-i-1] = '9';
+                } else if (s[n-i-1] == '9')
+                {
+                    s[n-i-1] = '6';
+                }
+                sReverse.Append(s[n-i-1]);
+            }
+ 
+            // Output
+            Console.WriteLine(sReverse);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -32,7 +32,7 @@ https://atcoder.jp/
 - AtCoder Beginner Contest 203 (2021.05.30 21:00, 100 minutes) (Comming)
 - AtCoder Regular Contest 121 (2021.05.29 21:00, 120 minutes) (Comming)
 - AtCoder Regular Contest 120 (2021.05.23 22:00, 120 minutes) (Comming)
-- AtCoder Beginner Contest 202 (2021.05.22 21:00, 100 minutes) (Comming)
+- AtCoder Beginner Contest 202 (2021.05.22 21:00, 100 minutes)
 - AtCoder Regular Contest 119 (2021.05.16 21:00, 120 minutes)
 - AtCoder Beginner Contest 201 (2021.05.15 21:00, 100 minutes)
 - AtCoder Beginner Contest 199 (2021.04.24 21:00, 100 minutes)

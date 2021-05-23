36e26f6d23aee9bce6af0766224ea80683c65183
Kangrok Kim <kimpro@uebermensch.tech>
2021-05-23 22:56:37 +0900

    Create A.cs

---
+++ AtCoder/Contest/Regular0120/A/A.cs
@@ -0,0 +1,63 @@
+﻿// AtCoder Regular Contest 120
+// 2021-05-23(Sun) 20:00 - 22:00, 120 minutes
+
+// A - Max Add
+
+/* Sample Input & Output
+3
+1 2 3
+
+2
+8
+19
+*/
+
+using System;
+using System.Linq;  // for .Select()
+
+namespace ARC120A
+{
+    class MainClass
+    {
+        public static void Main (string[] args) 
+        {
+
+            // Read data
+            int n = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            // f(a) operation of a_0 ~ a_k
+            for (int i = 0; i < n; i++)
+            {
+                int[] aSub = new int[i+1];              // Initialize for f(a)
+                int max = 0, sum = 0;
+                for (int j = 0; j <= i; j++)
+                {
+                    max = sum = 0;                      // Initialize for each j-th loop
+                    for (int k = 0; k <= i; k++)
+                    {
+                        if (j == 0)                     // Copy a[] to aSub[] only when j == 0
+                        {
+                            aSub[k] = a[k];
+                        }
+                        sum += aSub[k];
+                        max = Math.Max(max, aSub[k]);   // Get max
+                    }
+
+                    // Get sum and update aSub[]
+                    sum += max;                         // for saving additional loop to calculate sum
+                    aSub[j] += max;
+
+                    // Test
+                    Console.Write("(k = {0}) max : {1}, aSub : ", i, max);
+                    Array.ForEach(aSub, Console.Write);
+                    Console.WriteLine(", sum : {0}", sum);
+                }
+
+                // Output
+                Console.WriteLine(sum);
+
+            } // The end of the i loop
+        } // The end of main()
+    }
+}
---
+++ AtCoder/Contest/Regular0120/README.md
@@ -0,0 +1,74 @@
+## AtCoder Regular Contest 120
+2021-05-23(Sun) 20:00 - 22:00, 120 minutes  
+https://atcoder.jp/contests/arc120/
+
+
+### A - Max Add
+
+```cs
+/* Sample Input & Output
+3
+1 2 3
+
+2
+8
+19
+*/
+```
+
+```cs
+using System;
+using System.Linq;  // for .Select()
+```
+
+```cs
+// Read data
+int n = int.Parse(Console.ReadLine());
+int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+// f(a) operation of a_0 ~ a_k
+for (int i = 0; i < n; i++)
+{
+    int[] aSub = new int[i+1];              // Initialize for f(a)
+    int max = 0, sum = 0;
+    for (int j = 0; j <= i; j++)
+    {
+        max = sum = 0;                      // Initialize for each j-th loop
+        for (int k = 0; k <= i; k++)
+        {
+            if (j == 0)                     // Copy a[] to aSub[] only when j == 0
+            {
+                aSub[k] = a[k];
+            }
+            sum += aSub[k];
+            max = Math.Max(max, aSub[k]);   // Get max
+        }
+
+        // Get sum and update aSub[]
+        sum += max;                         // for saving additional loop to calculate sum
+        aSub[j] += max;
+
+        // Test
+        Console.Write("(k = {0}) max : {1}, aSub : ", i, max);
+        Array.ForEach(aSub, Console.Write);
+        Console.WriteLine(", sum : {0}", sum);
+    }
+
+    // Output
+    Console.WriteLine(sum);
+
+} // The end of the i loop
+```
+
+> 3  
+> 1 2 3
+
+> (k = 0) max : 1, aSub : 2, sum : 2  
+> 2  
+> (k = 1) max : 2, aSub : 32, sum : 5  
+> (k = 1) max : 3, aSub : 35, sum : 8  
+> 8  
+> (k = 2) max : 3, aSub : 423, sum : 9  
+> (k = 2) max : 4, aSub : 463, sum : 13  
+> (k = 2) max : 6, aSub : 469, sum : 19  
+> 19

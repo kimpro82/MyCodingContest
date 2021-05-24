0ff1758e14ff902646b92f11a12c42eecb5d28b3
Kangrok Kim <kimpro@uebermensch.tech>
2021-05-25 04:46:18 +0900

    Create 1330.cpp and 4 other .cpp files

---
+++ BaekJoon/Problems/Step/02/9498.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;                   // 82
+
+    if (a >= 90)
+    {
+        cout << "A" << endl;
+    } else if (a >= 80)
+    {
+        cout << "B" << endl;
+    } else if (a >= 70)
+    {
+        cout << "C" << endl;
+    } else if (a >= 60)
+    {
+        cout << "D" << endl;
+    } else
+    {
+        cout << "F" << endl;
+    }
+
+    return 0;
+}

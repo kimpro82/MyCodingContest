215ebeb8564d0b805484f61f0cadfba968353c71
kimpro82 <kimpro@uebermensch.tech>
2021-06-17 01:06:30 +0900

    Create 1065.cpp

---
+++ BaekJoon/Problems/Step/06/1065.cpp
@@ -0,0 +1,56 @@
+/* Sample input & output data
+110
+99
+
+1
+1
+
+210
+105
+
+1000
+144
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;      // n <= 1000
+    cin >> n;
+
+    int count = 0;
+    if (n < 100)
+    {
+        count = n;
+    } else
+    {
+        int a, b, c;
+        count = 99;
+        if (n == 1000)      // can determine 1000 is not
+        {
+            n = 999;
+        }
+        for (int i = 100; i <= n; i++)
+        {
+            a = i / 100;
+            b = (i % 100) / 10;
+            c = i % 10;
+            if ((b - a) == (c - b))
+            {
+                count++;
+            }
+            // cout << a << " " << b << " " << c << " " << count << endl;  // test
+        }
+    }
+    // else             // n == 1000
+    // {
+    //     count = 144; // given as a sample output …… crazy
+    // }
+
+    cout << count << endl;
+
+    return 0;
+}

b9b3afc237590334aff0caf9ebc88e2ffa4b7b10
Kangrok Kim <kimpro@uebermensch.tech>
2021-07-06 02:38:07 +0900

    Update E.kt

--- CodeForces/Contest/Kotlin Heroes Practice 7/E.kt
+++ CodeForces/Contest/Kotlin Heroes Practice 7/E.kt
@@ -24,21 +24,32 @@ fun main(args: Array<String>)
 {
     // input data into an array
     val n : Int = readLine()!!.toInt()
-    var a = readLine()!!.split(' ').map{it.toInt()}.toMutableList()
-
-    // remove i-th element, get the residuals' sum, and determine if good
-    var ans = ArrayList<Int>()
+    var a = readLine()!!.split(' ').map{it.toInt()} // Is there no way to make it an ArrayList directly?
+    var aArr = ArrayList<Int>()
     for (i in 0 until n)
     {
-        var b = a
-        b.removeAt(i)
-        if (b.sum() == b.max()!! * 2)
+        aArr.add(a[i])
+    }
+    // println(aArr.toString()) // test
+
+    // find elements that make it "good"
+    var ans = ArrayList<Int>()
+    for (j in 0 until n)
+    {
+        var temp = ArrayList<Int>()
+        temp.addAll(aArr)
+        temp.removeAt(j)
+        if (temp.sum() == temp.maxOrNull()!! * 2)   // 'max(): Int?' is deprecated
         {
-            ans.add(a[i])
+            ans.add(j+1)
         }
     }
 
     // output
     println(ans.size)
-    println(ans)
+    for (item in ans)
+    {
+        print("$item ")
+    }
+    println()
 }

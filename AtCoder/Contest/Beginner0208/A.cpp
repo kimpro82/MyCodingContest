fb78fb2d91e1857a85997d6c6aa8191f04927630
Kangrok Kim <kimpro1010@gmail.com>
2021-07-19 00:19:51 +0900

    AtCoder Beginner Contest 208 (#106)
    * Create A.cpp
    * Create B.cpp
    * Create C.cpp (TLE)
    
    * Update README.md

---
+++ AtCoder/Contest/Beginner0208/A.cpp
@@ -0,0 +1,36 @@
+﻿// AtCoder Beginner Contest 208
+// 2021-07-04(Sun) 21:00 - 22:40, 100 minutes
+
+// A - Rolling Dice
+
+/* Sample Input & Output
+2 11
+Yes
+
+2 13
+12
+
+100 600
+Yes
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a * 6 >= b && a <= b)
+    {
+        cout << "Yes" << '\n';
+    } else
+    {
+        cout << "No" << '\n';
+    }
+
+    return 0;
+}

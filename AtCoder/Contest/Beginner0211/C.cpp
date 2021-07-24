c9269d2c56c565af5ca2e8a48adf5bb58c975e7c
kimpro82 <kimpro@uebermensch.tech>
2021-07-24 23:40:10 +0900

    Create C.cpp (not completed)

---
+++ AtCoder/Contest/Beginner0211/C.cpp
@@ -0,0 +1,43 @@
+﻿// AtCoder Beginner Contest 211
+// 2021-07-24(Satn) 21:00 - 22:40, 100 minutes
+
+// C - chokudai
+
+/* Sample Input & Output
+chchokudai
+3
+
+atcoderrr
+0
+
+chokudaichokudaichokudai
+45
+*/
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    // Indput data
+    string chokudai = "chokudai", s;
+    // cout << chokudai[1] << endl;         // test : ok
+    cin >> s;
+
+    int count = 0;
+    for (int i = 0; i < s.size() - 8; i++)
+    {
+        string sub;
+        for (int j = i; j < s.size(); j++)
+        {
+            cout << s[j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

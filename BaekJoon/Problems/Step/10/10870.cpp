8790de9dbd77f04868e83e7354ab995b62a5dc97
kimpro82 <kimpro@uebermensch.tech>
2021-07-27 04:58:08 +0900

    Create 10870.cpp

---
+++ BaekJoon/Problems/Step/10/10870.cpp
@@ -0,0 +1,32 @@
+// [BOJ] 단계별로 풀어보기 > 10. 재귀
+
+// 10870. 피보나치 수 5
+
+/* Sample input & output data
+10
+55
+*/
+
+
+#include <iostream>
+
+using namespace std;
+#define endl '\n'
+
+int fibonacci(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n) << endl;
+
+    return 0;
+}

6bdaf8ce0d6a484f102d23fabb9dc1451d038733
kimpro82 <kimpro@uebermensch.tech>
2021-07-27 04:45:43 +0900

    Create 10872.cpp

---
+++ BaekJoon/Problems/Step/10/10872.cpp
@@ -0,0 +1,34 @@
+// [BOJ] 단계별로 풀어보기 > 10. 재귀
+
+// 10872. 팩토리얼
+
+/* Sample input & output data
+10
+3628800
+
+0
+1
+*/
+
+
+#include <iostream>
+
+using namespace std;
+#define endl '\n'
+
+int factorial(int n)
+{
+    if (n <= 1) return 1;
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    cout << factorial(N) << endl;
+
+    return 0;
+}

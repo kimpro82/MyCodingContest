27627616bde87af5b6ce5273c3391806c1c042e4
kimpro82 <kimpro@uebermensch.tech>
2021-07-31 23:00:44 +0900

    Create A.cpp

---
+++ AtCoder/Contest/Beginner0212/A.cpp
@@ -0,0 +1,37 @@
+﻿// AtCoder Beginner Contest 212
+// 2021-07-31(Sat) 21:00 - 22:40, 100 minutes
+
+// A - Alloy
+
+/* Sample Input & Output
+50 50
+Alloy
+
+100 0
+Gold
+
+0 100
+Silver
+
+100 2
+Alloy
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    if (A != 0 && B == 0) cout << "Gold" << endl;
+    else if (A == 0) cout << "Silver" << endl;
+    else cout << "Alloy" << endl;
+
+    return 0;
+}

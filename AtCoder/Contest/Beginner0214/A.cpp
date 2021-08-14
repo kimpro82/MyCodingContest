3605f48f901deebc25de09275dbc984c14973df9
Kangrok Kim <kimpro@uebermensch.tech>
2021-08-14 23:06:09 +0900

    Create A.cpp

---
+++ AtCoder/Contest/Beginner0214/A.cpp
@@ -0,0 +1,36 @@
+﻿// AtCoder Beginner Contest 214
+// 2021-08-14(Sat) 21:00 - 22:40, 100 minutes
+
+// A - New Generation ABC
+
+/* Sample Input & Output
+214
+8
+
+1
+4
+
+126
+6
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int N, ans;
+    cin >> N;
+
+    if (N <= 125) ans = 4;
+    else if (N <= 211) ans = 6;
+    else ans = 8;
+
+    cout << ans << endl;
+
+    return 0;
+}

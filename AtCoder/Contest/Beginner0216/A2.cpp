cd06c5461f3771ab32b5ca9fb7bbebe363250eee
Kangrok Kim <kimpro@uebermensch.tech>
2021-08-29 22:41:34 +0900

    Create A2.cpp

---
+++ AtCoder/Contest/Beginner0216/A2.cpp
@@ -0,0 +1,53 @@
+﻿// AtCoder Beginner Contest 216
+// 2021-08-29(Sun) 21:00 - 22:40, 100 minutes
+
+// A - Signed Difficulty
+
+/* Sample Input & Output
+15.8
+15+
+
+1.0
+1-
+
+12.5
+12
+*/
+
+
+#include <iostream>
+#include <cmath>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    string a;
+    cin >> a;
+
+    string x, y;
+    bool fraction = false;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == '.')
+        {
+            fraction = true;
+            continue;
+        }
+
+        if (fraction == false) x.push_back(a[i]);
+        else y.push_back(a[i]);
+    }
+
+    // test : ok
+    // cout << a << ' ' << x << ' ' << y << endl;
+
+    cout << x;
+    if (stoi(y) <= 2) cout << '-';
+    else if (stoi(y) >= 7) cout << '+';
+    cout << endl;
+
+    return 0;
+}

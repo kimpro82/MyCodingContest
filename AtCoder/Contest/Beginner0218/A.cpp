d37c8f5090284decbe44e2d3d5059c6ec46d12c7
kimpro82 <kimpro@uebermensch.tech>
2021-09-11 22:44:15 +0900

    Create A.cpp

---
+++ AtCoder/Contest/Beginner0218/A.cpp
@@ -0,0 +1,33 @@
+﻿// AtCoder Beginner Contest 218
+// 2021-09-11(Sat) 21:00 - 22:40, 100 minutes
+
+// A - Weather Forecast
+
+/* Sample Input & Output
+4
+oooxoox
+No
+
+7
+ooooooo
+Yes
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int N;
+    string S;
+    cin >> N >> S;
+
+    if (S[N-1]=='o') cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}

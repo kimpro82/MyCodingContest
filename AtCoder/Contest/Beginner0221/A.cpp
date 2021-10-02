0f18d75a114f300d3f5d2bf88de70d2626d636d0
kimpro82 <kimpro@uebermensch.tech>
2021-10-02 23:31:03 +0900

    Create A.cpp

---
+++ AtCoder/Contest/Beginner0221/A.cpp
@@ -0,0 +1,29 @@
+﻿// AtCoder Beginner Contest 221
+// 2021-10-02(Sat) 21:00 - 22:40, 100 minutes
+
+// A - Seismic magnitude scales
+
+/* Sample Input & Output
+6 4
+1024
+
+5 5
+1
+*/
+
+
+#include <iostream>
+#include <cmath>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << (long long) pow(32, A - B) << endl;
+
+    return 0;
+}

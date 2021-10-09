90562d05f5fc0a611a9a59face99f249ada79b40
Kangrok Kim <kimpro@uebermensch.tech>
2021-10-09 22:38:47 +0900

    Create A.cpp

---
+++ AtCoder/Contest/Beginner0222/A.cpp
@@ -0,0 +1,34 @@
+﻿// AtCoder Beginner Contest 222
+// 2021-10-09(Sat) 21:00 - 22:40, 100 minutes
+
+// A - Four Digits
+
+/* Sample Input & Output
+321
+0321
+
+7777
+7777
+
+1
+0001
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    string N;
+    cin >> N;
+    int zero = 4 - N.length();
+
+    for (int i = 0; i < zero; i++) cout << '0';
+    cout << N << endl;
+
+    return 0;
+}

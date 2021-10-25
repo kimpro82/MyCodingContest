dbf641ca6377743f6ac1c567d5a748aec7795661
Kangrok Kim <kimpro@uebermensch.tech>
2021-10-25 23:50:37 +0900

    Create 11729.cpp (not completed)

---
+++ BaekJoon/Problems/Step/10/11729.cpp
@@ -0,0 +1,42 @@
+// [BOJ] 단계별로 풀어보기 > 10. 재귀
+
+// 11729. 하노이 탑 이동 순서
+
+/* Sample input & output data
+3
+
+7
+1 3
+1 2
+3 2
+1 3
+2 1
+2 3
+1 3
+*/
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define endl '\n'
+
+int hanoi()
+{
+    // need more codes ……
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int k = pow(2, n) - 1;
+    cout << k << endl;
+
+    // need more codes ……
+
+    return 0;
+}

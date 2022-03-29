453c7784a950981e4dc3c9c7b144d83c4e87ce96
Kangrok Kim <kimpro@uebermensch.tech>
2022-03-30 01:03:17 +0900

    Update 10952.go

--- BaekJoon/Problems/Step/04/10952.go
+++ BaekJoon/Problems/Step/04/10952.go
@@ -3,11 +3,14 @@ import "fmt"
 
 func main() {
     var a, b int
+
     for true {
         fmt.Scanln(&a, &b)
+
         if a == 0 && b == 0 {
             break
         }
+
         fmt.Println(a + b)
     }
 }

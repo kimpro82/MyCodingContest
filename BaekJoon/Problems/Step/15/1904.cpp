83ef42cc5b69a81de9ef15d44f40638e00018ed2
Kangrok Kim <kimpro@uebermensch.tech>
2022-04-24 19:54:11 +0900

    Create 1904.cpp

---
+++ BaekJoon/Problems/Step/15/1904.cpp
@@ -0,0 +1,29 @@
+// 1904. 01타일
+
+/* Sample input & output data
+4
+
+5
+*/
+
+
+#include <iostream>
+#include <vector>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> dp {0, 1, 2};
+
+    // Find the recurrence relation!
+    for (int i = 3; i <= n; i++) dp.push_back((dp[i - 2] + dp[i - 1]) % 15746);     // pass if n < 3
+                                                                                    // don't forget the modulo!
+    cout << dp[n] << endl;
+
+    return 0;
+}

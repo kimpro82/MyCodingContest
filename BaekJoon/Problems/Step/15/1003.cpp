8e989f7fe80527bb059829073e1069360e21334d
Kangrok Kim <kimpro@uebermensch.tech>
2022-04-24 16:31:57 +0900

    Create 1003.cpp

---
+++ BaekJoon/Problems/Step/15/1003.cpp
@@ -0,0 +1,45 @@
+// 1003. 피보나치 함수
+
+/* Sample input & output data
+2
+6
+22
+
+5 8
+10946 17711
+*/
+
+
+#include <iostream>
+#include <vector>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+
+        // Count : Dynamic Programming
+        vector<pair<int, int>> dp;
+        for (int j = 0; j <= n; j++)
+        {
+            if (j == 0) dp.push_back(make_pair(1, 0));
+            else if (j == 1) dp.push_back(make_pair(0, 1));
+            else dp.push_back(make_pair(dp[j - 1].second, dp[j - 2].second + dp[j - 1].second));    // not ->second, but .second
+
+            // test
+            // cout << n << ' ' << j << ' ' << dp[j].first << ' ' << dp[j].second << endl;
+        }
+
+        // Output
+        cout << dp[n].first << ' ' << dp[n].second << endl;
+    }
+
+    return 0;
+}

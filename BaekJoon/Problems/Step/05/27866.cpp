de665aece8ddf3f42ff6a6afe1be142a85d4cbb3
Kangrok Kim <kimpro@uebermensch.tech>
2023-06-18 16:22:51 +0900

    Create 27866.cpp

---
+++ BaekJoon/Problems/Step/05/27866.cpp
@@ -0,0 +1,29 @@
+// 27866. 문자와 문자열
+// 2023.06.18
+
+/* Sample input & output data
+Sprout
+3
+
+r
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    // Input
+    string s;
+    int idx;
+    std::cin >> s >> idx;
+
+    // Output
+    cout << s[idx-1] << endl;
+
+    return 0;
+}

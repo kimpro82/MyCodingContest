290604e15cd45d5dc34bab78a3154108868d2da0
Kangrok Kim <kimpro@uebermensch.tech>
2023-07-03 02:32:10 +0900

    Create 27323.cpp

---
+++ BaekJoon/Problems/Step/10/27323.cpp
@@ -0,0 +1,27 @@
+// 27323. 長方形(Rectangle, 직사각형)
+// 2023.07.02
+
+/* Sample input & output data
+2
+3
+
+6
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    // Input
+    int a, b;
+    cin >> a >> b;
+
+    // Output
+    cout << a * b << endl;
+    return 0;
+}

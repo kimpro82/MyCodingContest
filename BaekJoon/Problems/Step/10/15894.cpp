83ed4428a385a84ee74314f5aa16d60b517ccc0d
Kangrok Kim <kimpro@uebermensch.tech>
2023-07-03 02:38:15 +0900

    Create 15894.cpp

---
+++ BaekJoon/Problems/Step/10/15894.cpp
@@ -0,0 +1,28 @@
+// 15894. 수학은 체육과목 입니다
+// 2023.07.02
+
+/* Sample input & output data
+3
+
+12
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    // Input
+    ll n;
+    cin >> n;
+
+    // Output
+    cout << n * 4 << endl;
+
+    return 0;
+}

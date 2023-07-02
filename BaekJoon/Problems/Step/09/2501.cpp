2877e69085ac952e15cd9c926df3ad5745a13aa8
Kangrok Kim <kimpro@uebermensch.tech>
2023-07-03 01:33:04 +0900

    Create 2501.cpp

---
+++ BaekJoon/Problems/Step/09/2501.cpp
@@ -0,0 +1,50 @@
+// 2501. 약수 구하기
+// 2023.07.02
+
+/* Sample input & output data
+6 3
+
+3
+*/
+
+
+#include <iostream>
+#include <vector>
+
+// #define test
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    // Input
+    cin >> a >> b;
+
+    vector<int> v;
+    int cnt = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        if (a % i == 0)
+        {
+            cnt++;
+
+            // Test
+            #ifdef test
+                printf("i:%d remainder:%d cnt:%d\n", i, a%i, cnt);
+            #endif
+
+            // Operate and Output
+            if (cnt == b)
+            {
+                cout << i << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << 0 << endl;
+    return 0;
+}

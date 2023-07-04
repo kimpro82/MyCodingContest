9284c5b7ce0b1a652c6364a36102234ed38f7e44
Kangrok Kim <kimpro@uebermensch.tech>
2023-07-05 02:17:27 +0900

    Create 24262_2.cpp

---
+++ BaekJoon/Problems/Step/11/24262_2.cpp
@@ -0,0 +1,29 @@
+// 24262. 알고리즘 수업 - 알고리즘의 수행 시간 1
+// 2023.07.04
+
+/* Sample input & output data
+1
+
+1
+0
+*/
+
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    // Input
+    int n;
+    cin >> n;
+
+    // Output
+    cout << 1 << endl;
+    cout << 0 << endl;
+
+    return 0;
+}

b2f1e8e625ceea37ebbcd6b83dc9e1ef7347fb5a
Kangrok Kim <kimpro@uebermensch.tech>
2023-12-22 01:53:58 +0900

    Update 10926.rs

--- BaekJoon/Problems/Step/01/10926.rs
+++ BaekJoon/Problems/Step/01/10926.rs
@@ -1,4 +1,4 @@
-// 18108. 1998년생인 내가 태국에서는 2541년생?!
+// 10926. ??!
 // 2023.12.21
 
 use std::io;
@@ -6,6 +6,7 @@ use std::io;
 fn main()
 {
     let mut input = String::new();
+
     io::stdin().read_line(&mut input).unwrap();
 
     println!("{}??!", input.replace("\n", ""));
